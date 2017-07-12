ba0dbfdd12fbe2c3bb5d1c1cb830fb553916f62f
Joel Guizar <joelguizar@gmail.com>
2017-07-11 22:49:58 -0700

    cookieparser bodyparser

--- server.js
+++ server.js
@@ -3,7 +3,10 @@ const app = express();
 const CP = require('cookieparser');
 const BP = require('bodyparser');
 
-app.listen("4000");
+app.CP();
+app.BP();
+
+app.listen(4000);
 
 app.get("/", function(req,res){
   res.send("hey");
