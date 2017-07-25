4445424132cba68b53868b4bcbec8dfbb30596d4
Joel Guizar <joelguizar@gmail.com>
2017-07-24 21:44:57 -0700

    schema controller plan

--- controller.js
+++ controller.js
@@ -1,6 +1,12 @@
 const controller = function () =>{
+  //grabAll using mongoose
 
+  //grabOne using mongoose
+
+  //one more mongoose
+
+  //import schema
 }
 
 
-module.exports = controller
+module.exports = controller;
